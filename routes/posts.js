7326194c6f1c86af1b84895765f28130ece861d7
abdou innouva <abdouinnouva@gmail.com>
2020-03-10 17:57:44 +0100

    install multer express

--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,14 @@ const express = require('express');
 const PostsController = require('../controllers/PostsController');
 
 const router = express.Router();
+//import multer
+var multer  = require('multer')
+
+//create path file img multer
+var upload = multer({ dest: 'uploads/' })
 
 router.get('', PostsController.getAllPosts);
-router.post('', PostsController.addPosts);
+router.post('', upload.single('avatar'), PostsController.addPosts);
 router.put('/:id', PostsController.updatePost);
 //router.get('/:id/edit', PostsController.editPost)
 router.get('/:id', PostsController.showOnePost)
