113b1f1ba4724844d4c38fef3439181c943efd02
hajar <hajr.ennassiry@gmail.com>
2020-03-10 09:51:58 +0100

    comment controller

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('project_nodejs_tp', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    'port': 3307
 });
 
 module.exports = connection;
