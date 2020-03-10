1d1b239a811976dbd54e92e21eab76c56e0fe1d1
MeryemeEdd <meryemeeddoug@gmail.com>
2020-03-10 09:24:50 +0100

    update

--- routes/categories.js
+++ routes/categories.js
@@ -5,11 +5,13 @@ const router = express.Router();
 
 //router.get('/create', CategoryController.createCategory);
 router.get('', CategoryController.getAllCategories);
-// router.post('', CategoryController.storeCategory);
-// router.put('/:id', CategoryController.updateCategory);
-//router.get('/:id/edit', CategoryController.editCategory)
-// router.get('/:id', CategoryController.showOneCategory)
-// router.patch('/:id', CategoryController.patchCategory)
-// router.delete('/:id', CategoryController.deleteCategory)
+router.post('', CategoryController.storeCategory);
+router.put('/:id', CategoryController.updateCategory);
+router.get('/:id', CategoryController.showOneCategory)
+router.delete('/:id', CategoryController.deleteCategory)
+// router.get('/:id/edit', CategoryController.editCategory)
+
+router.patch('/:id', CategoryController.patchCategory)
+
 
 module.exports = router;
